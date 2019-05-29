d83f2fc0202b543c3627c692fe6af5ab9e06f46f
Terrell Page <tpage46@gmail.com>
2019-05-29 08:02:53 -0600

    added .gitignore

--- test/edu/cnm/deepdive/LotteryTest.java
+++ test/edu/cnm/deepdive/LotteryTest.java
@@ -2,6 +2,12 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class LotteryTest {
 
+  @Test
+  void Lottery() {
+    
+  }
 }
